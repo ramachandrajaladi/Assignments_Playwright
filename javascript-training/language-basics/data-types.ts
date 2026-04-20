42dafea86dff6cf7d15b0fe2bd6bd796db3df1f9
ramachandra <ramachandrajaladi101@gmail.com>
2026-04-20 16:01:33 +0530

    Data Types

---
+++ javascript-training/language-basics/data-types.ts
@@ -0,0 +1,79 @@
+// 1. Temperature of a city in degrees Celsius
+let temp:number = 25.5
+console.log(`Temperature of a city in degrees Celsius: ${temp}`)
+
+// 2. whether a customer has placed an order
+let order:boolean = true
+console.log(`whether a customer has placed an order: ${order}`)
+
+// 3. Person's phn number
+let myNumber:String = "986-648-4364"
+console.log(`Person's phn number: ${myNumber}`)
+
+// 4. Amount of money in a customer's bank account
+let amount:number = 1000.40
+console.log(`Amount of money in a customer's bank account: ${amount}`)
+
+// 5. Person's email address
+const emailAddress:String = "testanalyast@gmail.com"
+console.log(`Person's email address: ${emailAddress}`)
+
+// 6. Coordinates of location (lattitude, longitude)
+let coordinates: [number, number] = [17.3850, 78.4867]
+console.log(`Coordinates of location (lattitude, longitude): ${coordinates}`)
+
+// 7. Person martial status
+let martialStatus:boolean = true
+console.log(`Person martial status: ${martialStatus}`)
+
+// 8. Person's occupation
+let occupation:String = "Software Engineer"
+console.log(`Person's occupation: ${occupation}`)
+
+// 9. Person's favourite colour
+let colour:String = "Blue"
+console.log(`Person's favourite colour: ${colour}`)
+
+//10. current year: 2023
+let year:number = 2023
+console.log(`Current year: ${year}`)
+
+// 11. Number of followers in a social media: 1,00,000
+let followers:string = "1,00,000"
+console.log(`Number of followers in a social media: ${followers}`)
+
+//12. Rating of a movie
+let ratingMovie:number = 7.5
+console.log(`Rating of a movie: ${ratingMovie}`)
+
+//13. Person's blood group
+let bloodGroup:String = 'A+'
+console.log(`Person's blood group: ${bloodGroup}`)
+
+//14. Title of a book: "To Kill a Mockingbird"
+let bookName:String = "To Kill a Mockingbird"
+console.log(`Title of a book: ${bookName}`)
+
+//15. Number of companied in a company: 500
+let no_ofEmployees:number = 590
+console.log(`Number of employees in a company: ${no_ofEmployees}`)
+
+//16. Time of an event: 2:30 PM
+let eventTime: String = "2:30 PM"
+console.log(`Time of an event: ${eventTime}`)
+
+// 17. Name of a Country United States
+const country:string = "United States"
+console.log(`Name of a Country United States: ${country}`)
+
+//18. Person's eye colour: Blue
+const eyeColour:string = "Brown"
+console.log(`Person's eye colour: ${eyeColour}`)
+
+//19. Person's Birth place
+const birthPlace:String = "Chilakaluri Pet"
+console.log(`Person's Birth place: ${birthPlace}`)
+
+//20. Distance between two cities
+let distances:number = 200.4
+console.log(`Distance between two cities: ${distances}`)
